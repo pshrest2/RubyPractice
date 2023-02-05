41de333a83a18be38e0849cf042a391b5bcaec90
pshrestha <pranaya.shrestha2@gmail.com>
2023-02-05 12:31:36 -0600

    let active record deal with password and password_confirmation not match error handling

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -7,17 +7,15 @@ class PasswordController < ApplicationController
     def update
         if !Current.user.authenticate(password_params[:current_password])
             redirect_to password_path, :flash => { :danger => "Current password is invalid" }
-        elsif password_params[:new_password] != password_params[:new_password_confirmation]
-            redirect_to password_path, :flash => { :danger => "Passwords do not match" }
-        elsif Current.user.update(:password => password_params[:new_password], :password_confirmation => password_params[:new_password_confirmation])
+        elsif Current.user.update(:password => password_params[:password], :password_confirmation => password_params[:password_confirmation])
             redirect_to root_path, :flash => {:success => "Password updated"}
         else
-            render :new
+            render :new, status: :unprocessable_entity
         end
     end
 
     private
     def password_params
-        params.require(:user).permit(:current_password, :new_password, :new_password_confirmation)
+        params.require(:user).permit(:current_password, :password, :password_confirmation)
     end
 end
